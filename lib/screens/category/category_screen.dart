e657216b7f5be733e53c3c197d56fdee3c6d3392
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2022-11-08 15:14:36 +0300

    feat: adding base screens

---
+++ lib/screens/category/category_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatefulWidget {
+  const CategoryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Category"),
+      ),
+      body: const Center(
+        child: Text("category"),
+      ),
+    );
+  }
+}
