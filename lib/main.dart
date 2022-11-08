91ee30aadb4120ebdafdcebe2b98c5e455886794
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2022-11-08 15:15:02 +0300

    feat: adding bottomNavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:maydan/utilities/app_utilities.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'screens/category/category_screen.dart';
+import 'screens/favorite/favorite_screen.dart';
+import 'screens/home/home_screen.dart';
+import 'screens/post/post_screen.dart';
+import 'screens/profile/profile_screen.dart';
+import 'utilities/app_utilities.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,14 +36,50 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+  final List<Widget> _children = [
+    const HomeScreen(),
+    const CategoryScreen(),
+    const PostScreen(),
+    const FavoriteScreen(),
+    const ProfileScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Maydan"),
-      ),
-      body: const Center(
-        child: Text("Hello From Maydan"),
+      body: _children[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (pos) => setState(() {
+          currentIndex = pos;
+        }),
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Category",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/post.svg',
+              semanticsLabel: '',
+            ),
+            label: "",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Order",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.format_list_bulleted_rounded),
+            label: "More",
+          ),
+        ],
       ),
     );
   }
