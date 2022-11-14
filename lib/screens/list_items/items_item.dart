27e78f36ae8c9aeb15bc5b1c9b98f0a1607c335e
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2022-11-14 16:07:08 +0300

    feat: adding list items

---
+++ lib/screens/list_items/items_item.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../utilities/app_utilities.dart';
+
+class ItemsItem extends StatelessWidget {
+  const ItemsItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      color: const Color(0xFFE5E5E5),
+      margin: const EdgeInsetsDirectional.only(start: 4, end: 4, bottom: 16),
+      child: Column(
+        children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadiusDirectional.only(
+                  topStart: Radius.circular(20), topEnd: Radius.circular(20)),
+            ),
+            child: Stack(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 155,
+                  child: Image.network(
+                    "https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional.topEnd,
+                  child: IconButton(
+                    icon: Stack(
+                      children: [
+                        SvgPicture.asset(
+                          favBoarderSvg,
+                          semanticsLabel: '',
+                        ),
+                        Container(
+                          margin: const EdgeInsetsDirectional.all(8),
+                          child: Align(
+                            child: SvgPicture.asset(
+                              mainFullFavoriteBottomNavigationSvg,
+                              color: appColor,
+                              semanticsLabel: '',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Padding(
+                    padding: EdgeInsetsDirectional.only(start: 8, top: 8),
+                    child: Text(
+                      "item Home SubItem ssd",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400, color: Colors.black),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.only(start: 8, bottom: 4),
+                    child: Text(
+                      "25/12/2022",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400, color: Colors.black),
+                    ),
+                  ),
+                ],
+              )),
+              const Padding(
+                padding: EdgeInsetsDirectional.only(start: 8, end: 8),
+                child: Text("10\$"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/list_items/list_items_screen.dart
+++ lib/screens/list_items/list_items_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'items_item.dart';
+
 class ListItemsScreen extends StatefulWidget {
   const ListItemsScreen({Key? key}) : super(key: key);
 
@@ -21,6 +23,15 @@ class _ListItemsScreenState extends State<ListItemsScreen> {
           color: Colors.black,
         ),
         backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsetsDirectional.only(start: 4, end: 4),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, mainAxisExtent: 220),
+          itemBuilder: (BuildContext context, int index) => const ItemsItem(),
+          itemCount: 30,
+        ),
       ),
     );
   }
