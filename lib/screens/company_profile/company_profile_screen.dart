d14a99cfb44efd41b2068ee5febfe7445b36f4d4
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2022-11-17 10:03:49 +0300

    feat: adding company account

--- lib/screens/company_profile/company_profile_screen.dart
+++ lib/screens/company_profile/company_profile_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:maydan/utilities/app_utilities.dart';
+
+import '../item_detail/detail_item_meta_data.dart';
+import '../list_items/items_item.dart';
 
 class CompanyProfileScreen extends StatefulWidget {
   const CompanyProfileScreen({Key? key}) : super(key: key);
@@ -70,6 +74,73 @@ class _CompanyProfileScreenState extends State<CompanyProfileScreen> {
                       child: Text('Item 4'),
                     ),
                   ]),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            margin: const EdgeInsetsDirectional.only(start: 8, end: 8),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Image.network(
+                width: double.infinity,
+                'https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80',
+                fit: BoxFit.cover,
+                errorBuilder: (_, __, ___) => const Image(
+                  image: AssetImage(mainProfileBottomNavigationSvg),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsetsDirectional.all(8),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.only(
+                        topStart: Radius.circular(10),
+                        topEnd: Radius.circular(10)),
+                    color: appColor,
+                  ),
+                  child: const Center(
+                    child: Text("Cow"),
+                  ),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => const DetailItemMetaData(),
+                  itemCount: 3,
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("Products related to X company"),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.only(start: 4, end: 4),
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, mainAxisExtent: 220),
+              itemBuilder: (BuildContext context, int index) =>
+                  const ItemsItem(),
+              itemCount: 3,
+            ),
+          ),
         ],
       ),
     );
