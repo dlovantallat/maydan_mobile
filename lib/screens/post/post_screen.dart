97f74ae61aa23556343bcb2805618724cea490fb
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2022-12-20 14:42:56 +0300

    feat: adding reset of post fields

--- lib/screens/post/post_screen.dart
+++ lib/screens/post/post_screen.dart
@@ -18,6 +18,18 @@ class _PostScreenState extends State<PostScreen> {
   final List<CategoryDrop> _dropdownCategoriesDrop = [];
   late CategoryDrop _dropdownCategoryValue;
 
+  final List<String> _dropdownDurationDrop = ["7", "15", "30"];
+  String _dropdownDurationValue = "7";
+
+  final List<String> _dropdownPriceDrop = ["IQ", "USD"];
+  String _dropdownPriceValue = "IQ";
+
+  final List<String> _dropdownCityDrop = ["Erbil", "Sulaymany", "Duhok"];
+  String _dropdownCityValue = "Erbil";
+
+  final List<String> _dropdownDistrictDrop = ["Nawand", "Golden"];
+  String _dropdownDistrictValue = "Nawand";
+
   final List<SubCategoryDrop> _dropdownSubCategoriesDrop = [
     SubCategoryDrop("-1", "Select")
   ];
@@ -76,6 +88,12 @@ class _PostScreenState extends State<PostScreen> {
       isSubLoaded = true;
     });
   }
+
+  void openGallery() {
+    print("hello from gallery");
+  }
+
+  void save() async {}
 
   @override
   Widget build(BuildContext context) {
@@ -194,6 +212,259 @@ class _PostScreenState extends State<PostScreen> {
                       : Container(
                           child: const Text("Please Select another category"),
                         ),
+              const Padding(
+                padding: EdgeInsetsDirectional.only(top: 12, bottom: 4),
+                child: Text("Ad photos"),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    onTap: openGallery,
+                    child: Card(
+                      elevation: 8,
+                      child: Container(
+                          margin: const EdgeInsetsDirectional.only(
+                              start: 40, end: 40, top: 30, bottom: 30),
+                          child: SvgPicture.asset(
+                            editProfileSvg,
+                            height: 50,
+                            width: 30,
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsetsDirectional.only(
+                  top: 12,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                height: 70,
+                child: const Padding(
+                  padding: EdgeInsetsDirectional.all(8),
+                  child: Text("Description"),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsetsDirectional.only(top: 12, bottom: 4),
+                        child: Text("Price"),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                              color: Colors.black,
+                              style: BorderStyle.solid,
+                              width: 1),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              start: 8, end: 8),
+                          child: DropdownButton(
+                              underline: const SizedBox(),
+                              items: _dropdownPriceDrop
+                                  .map((value) => DropdownMenuItem(
+                                        value: value,
+                                        child: Text(value),
+                                      ))
+                                  .toList(),
+                              value: _dropdownPriceValue,
+                              onChanged: (i) {
+                                setState(() {
+                                  _dropdownPriceValue = i!;
+                                });
+
+                                print(_dropdownPriceValue);
+                              }),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsetsDirectional.only(top: 12, bottom: 4),
+                        child: Text("Duration"),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                              color: Colors.black,
+                              style: BorderStyle.solid,
+                              width: 1),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              start: 8, end: 8),
+                          child: DropdownButton(
+                              underline: const SizedBox(),
+                              items: _dropdownDurationDrop
+                                  .map((value) => DropdownMenuItem(
+                                        value: value,
+                                        child: Text(value),
+                                      ))
+                                  .toList(),
+                              value: _dropdownDurationValue,
+                              onChanged: (i) {
+                                setState(() {
+                                  _dropdownDurationValue = i!;
+                                });
+
+                                print(_dropdownDurationValue);
+                              }),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsetsDirectional.only(bottom: 8, top: 20),
+                child: Text("Seller name"),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsetsDirectional.only(bottom: 8, top: 20),
+                child: Text("Phone number"),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsetsDirectional.only(top: 12, bottom: 4),
+                        child: Text("City"),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                              color: Colors.black,
+                              style: BorderStyle.solid,
+                              width: 1),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              start: 8, end: 8),
+                          child: DropdownButton(
+                              underline: const SizedBox(),
+                              items: _dropdownCityDrop
+                                  .map((value) => DropdownMenuItem(
+                                        value: value,
+                                        child: Text(value),
+                                      ))
+                                  .toList(),
+                              value: _dropdownCityValue,
+                              onChanged: (i) {
+                                setState(() {
+                                  _dropdownCityValue = i!;
+                                });
+
+                                print(_dropdownCityValue);
+                              }),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsetsDirectional.only(top: 12, bottom: 4),
+                        child: Text("District"),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                              color: Colors.black,
+                              style: BorderStyle.solid,
+                              width: 1),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              start: 8, end: 8),
+                          child: DropdownButton(
+                              underline: const SizedBox(),
+                              items: _dropdownDistrictDrop
+                                  .map((value) => DropdownMenuItem(
+                                        value: value,
+                                        child: Text(value),
+                                      ))
+                                  .toList(),
+                              value: _dropdownDistrictValue,
+                              onChanged: (i) {
+                                setState(() {
+                                  _dropdownDistrictValue = i!;
+                                });
+
+                                print(_dropdownDistrictValue);
+                              }),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.only(top: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    OutlinedButton(
+                        onPressed: () {}, child: const Text("clear")),
+                    ElevatedButton(
+                        onPressed: save, child: const Text("Confirm")),
+                  ],
+                ),
+              ),
             ],
           ),
         );
