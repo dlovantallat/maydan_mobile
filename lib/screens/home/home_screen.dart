862598f6eb83b96c75c0566ce1095762db0b9fed
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2022-12-22 12:03:55 +0300

    feat: adding drawer to home page

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -60,6 +60,214 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         itemCount: 5,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(color: appColor),
+              child: Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsetsDirectional.only(top: 8, bottom: 32),
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsetsDirectional.only(end: 16),
+                            height: 40,
+                            width: 40,
+                            color: const Color(0x49000000),
+                          ),
+                          const Text(
+                            'My Profile',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Kurdish',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'Arabic',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'English',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsetsDirectional.only(start: 16, end: 16),
+              child: Column(
+                children: [
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('About Us'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Company accounts'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('My Ads'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('My favourites'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Help'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('FAQ'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Logout'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    shape: const Border(
+                      bottom: BorderSide(color: Color(0x5E000000)),
+                    ),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Term & Conditions'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Privacy Policy'),
+                        Icon(Icons.arrow_forward_ios_outlined)
+                      ],
+                    ),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
