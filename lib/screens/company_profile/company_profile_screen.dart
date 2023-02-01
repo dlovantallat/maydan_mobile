93cae57cc9ba695ef41abd70f436d1ab7e309440
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2023-02-01 09:54:32 +0300

    fix: company social detail

--- lib/screens/company_profile/company_profile_screen.dart
+++ lib/screens/company_profile/company_profile_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maydan/utilities/app_utilities.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../profile/profile.dart';
 import 'company_widget.dart';
@@ -13,6 +16,14 @@ class CompanyProfileScreen extends StatefulWidget {
 }
 
 class _CompanyProfileScreenState extends State<CompanyProfileScreen> {
+  Future<void> _launchUrl(String adsUrl) async {
+    final Uri url = Uri.parse(adsUrl);
+
+    if (!await launchUrl(url)) {
+      setSnackBar(context, 'Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +33,47 @@ class _CompanyProfileScreenState extends State<CompanyProfileScreen> {
           color: Colors.black,
         ),
         backgroundColor: Colors.transparent,
+        actions: [
+          if (widget.data.facebook != "")
+            Padding(
+              padding: const EdgeInsetsDirectional.only(end: 16),
+              child: InkWell(
+                onTap: () {
+                  _launchUrl(widget.data.facebook!);
+                },
+                child: SvgPicture.asset(
+                  fbSvg,
+                  semanticsLabel: '',
+                ),
+              ),
+            ),
+          if (widget.data.youtube != "")
+            Padding(
+              padding: const EdgeInsetsDirectional.only(end: 16),
+              child: InkWell(
+                onTap: () {
+                  _launchUrl(widget.data.youtube!);
+                },
+                child: SvgPicture.asset(
+                  ytSvg,
+                  semanticsLabel: '',
+                ),
+              ),
+            ),
+          if (widget.data.whatsapp != "")
+            Padding(
+              padding: const EdgeInsetsDirectional.only(end: 16),
+              child: InkWell(
+                onTap: () {
+                  _launchUrl(widget.data.instagram!);
+                },
+                child: SvgPicture.asset(
+                  instagramSvg,
+                  semanticsLabel: '',
+                ),
+              ),
+            )
+        ],
       ),
       body: CompanyWidget(data: widget.data),
     );
