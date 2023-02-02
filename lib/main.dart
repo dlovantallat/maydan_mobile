8be261022e7e26c634ceb1dcffc3bf8fdec4c20e
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2023-02-02 11:16:00 +0300

    fix: android background notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
@@ -69,6 +71,9 @@ Future<void> main() async {
   }
 
   // getDeviceToken();
+  if (Platform.isAndroid) {
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  }
 
   servicesLocator();
 
@@ -86,6 +91,15 @@ Future<void> main() async {
       ),
     ),
   );
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
 }
 
 void getDeviceToken() async {
