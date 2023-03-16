7291818d1949af588722f8ad1c4fd4f22f8d8575
dilovantalaat45 <110896608+dilovantalaat45@users.noreply.github.com>
2023-03-16 15:43:09 +0300

    fix: start up language setting

--- lib/screens/on_boarding/language_start_up_screen.dart
+++ lib/screens/on_boarding/language_start_up_screen.dart
@@ -18,47 +18,24 @@ class LanguageStartUpScreen extends StatefulWidget {
 }
 
 class _LanguageStartUpScreenState extends State<LanguageStartUpScreen> {
-  bool isEnglish = false;
-  bool isKurdish = false;
-  bool isArabic = false;
-
-  @override
-  void initState() {
-    checker("en");
-
-    super.initState();
-  }
+  final ff = MaterialStateProperty.resolveWith<Color?>(
+    (Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return const Color(0x81acce52);
+      }
+      return Colors.white; // Use the component's default.
+    },
+  );
 
   setLanguage(String key) async {
     final provider = Provider.of<LocaleProvider>(context, listen: false);
     provider.setLocale(key);
 
-    checker(key);
-
     SharedPreferences preferences = await SharedPreferences.getInstance();
     preferences.setString(languageKey, key);
-  }
-
-  void checker(String key) {
-    if (key == "en") {
-      setState(() {
-        isEnglish = true;
-        isKurdish = false;
-        isArabic = false;
-      });
-    } else if (key == "ar") {
-      setState(() {
-        isEnglish = false;
-        isKurdish = false;
-        isArabic = true;
-      });
-    } else if (key == "fa") {
-      setState(() {
-        isEnglish = false;
-        isKurdish = true;
-        isArabic = false;
-      });
-    }
+    if (!mounted) return;
+    Navigator.push(
+        context, MaterialPageRoute(builder: (_) => const OnBoardingScreen()));
   }
 
   @override
@@ -66,113 +43,61 @@ class _LanguageStartUpScreenState extends State<LanguageStartUpScreen> {
     return Scaffold(
       backgroundColor: appColor,
       body: SafeArea(
-        child: Stack(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Padding(
+              padding: const EdgeInsetsDirectional.only(bottom: 36),
+              child: SvgPicture.asset(
+                homeLogoSvg,
+                height: 100,
+                semanticsLabel: '',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(bottom: 36),
-                  child: SvgPicture.asset(
-                    homeLogoSvg,
-                    height: 100,
-                    semanticsLabel: '',
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: ff,
+                  ),
+                  onPressed: () {
+                    setLanguage("fa");
+                    Get.updateLocale(const Locale("fa"));
+                  },
+                  child: Text(
+                    AppLocalizations.of(context)!.home_drawer_kurdish,
+                    style: const TextStyle(color: appColor),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.resolveWith<Color?>(
-                          (Set<MaterialState> states) {
-                            if (states.contains(MaterialState.disabled)) {
-                              return const Color(0x81acce52);
-                            }
-                            return Colors.white; // Use the component's default.
-                          },
-                        ),
-                      ),
-                      onPressed: !isKurdish
-                          ? () {
-                              setLanguage("fa");
-                              Get.updateLocale(const Locale("fa"));
-                            }
-                          : null,
-                      child: Text(
-                        AppLocalizations.of(context)!.home_drawer_kurdish,
-                        style: TextStyle(
-                            color: isKurdish ? Colors.white : appColor),
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.resolveWith<Color?>(
-                          (Set<MaterialState> states) {
-                            if (states.contains(MaterialState.disabled)) {
-                              return const Color(0x81acce52);
-                            }
-                            return Colors.white; // Use the component's default.
-                          },
-                        ),
-                      ),
-                      onPressed: !isArabic
-                          ? () {
-                              setLanguage("ar");
-                              Get.updateLocale(const Locale("ar"));
-                            }
-                          : null,
-                      child: Text(
-                        AppLocalizations.of(context)!.home_drawer_arabic,
-                        style: TextStyle(
-                            color: isArabic ? Colors.white : appColor),
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.resolveWith<Color?>(
-                          (Set<MaterialState> states) {
-                            if (states.contains(MaterialState.disabled)) {
-                              return const Color(0x81acce52);
-                            }
-                            return Colors.white; // Use the component's default.
-                          },
-                        ),
-                      ),
-                      onPressed: !isEnglish
-                          ? () {
-                              setLanguage("en");
-                              Get.updateLocale(const Locale("en"));
-                            }
-                          : null,
-                      child: Text(
-                        AppLocalizations.of(context)!.home_drawer_english,
-                        style: TextStyle(
-                            color: isEnglish ? Colors.white : appColor),
-                      ),
-                    ),
-                  ],
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: ff,
+                  ),
+                  onPressed: () {
+                    setLanguage("ar");
+                    Get.updateLocale(const Locale("ar"));
+                  },
+                  child: Text(
+                    AppLocalizations.of(context)!.home_drawer_arabic,
+                    style: const TextStyle(color: appColor),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: ff,
+                  ),
+                  onPressed: () {
+                    setLanguage("en");
+                    Get.updateLocale(const Locale("en"));
+                  },
+                  child: Text(
+                    AppLocalizations.of(context)!.home_drawer_english,
+                    style: const TextStyle(color: appColor),
+                  ),
                 ),
               ],
             ),
-            PositionedDirectional(
-              end: 16,
-              bottom: 16,
-              child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => const OnBoardingScreen()));
-                  },
-                  child: Text(
-                    AppLocalizations.of(context)!.on_board_skip,
-                    style: const TextStyle(color: Colors.white),
-                  )),
-            )
           ],
         ),
       ),
